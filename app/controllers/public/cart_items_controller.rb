1b4e6c02d49881140fca31602011a04d4be0dfdc
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-08-23 15:31:45 +0000

    8.23-1

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,22 +4,42 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total_price = @cart_items.inject(0) do |sum, cart_item|
+      sum + cart_item.item.price * cart_item.amount
+    end
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    if @cart_item.amount == "個数選択"
-      redirect_to item_path(@cart_item.item_id), alert: '個数を選択してください。'
-      return
-    end
-    if @cart_item.save
-      flash[:notice] = "カートに商品を追加されました。"
-      redirect_to cart_items_path
+    # 1. 追加した商品がカート内に存在するかの判別
+    existing_cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+
+    if existing_cart_item
+      # 存在した場合
+      # 2. カート内の個数をフォームから送られた個数分追加する
+      new_amount = existing_cart_item.amount + params[:cart_item][:amount].to_i
+      existing_cart_item.update(amount: new_amount)
+      flash[:notice] = "カート内の商品数量を更新しました。"
     else
-      flash[:notice] = "カートに商品を追加できませんでした。"
-      redirect_to item_path(@cart_item.item_id)
+      # 存在しなかった場合
+      # カートモデルにレコードを新規作成する
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.amount.nil? || @cart_item.amount == 0
+        flash[:notice] = "個数を選択してください。"
+        redirect_to item_path(@cart_item.item_id)
+        return
+      end
+      if @cart_item.save
+        flash[:notice] = "カートに商品を追加されました。"
+      else
+        flash[:notice] = "カートに商品を追加できませんでした。"
+        redirect_to item_path(@cart_item.item_id)
+        return
+      end
     end
+    redirect_to cart_items_path
   end
+
 
 
   def update
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  TAX_RATE = 0.08
+  TAX_RATE = 0.1
 
   def tax_included_price(price)
     (price * (1 + TAX_RATE)).round
