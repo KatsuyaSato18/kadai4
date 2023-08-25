b9276be84a5c96dc6a0260a6eb26da32be8c6933
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-08-25 15:02:51 +0000

    8.25-1

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,13 +1,31 @@
 class Public::OrdersController < ApplicationController
+  before_action :set_order, only: [:update]
+
   def new
+    @order = Order.new
+    @customer = current_customer
   end
 
-  def thanks
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    if @order.save
+      # 保存に成功した場合の処理
+      redirect_to order_path(@order)
+      flash[:notice] = "注文が完了しました。"
+    else
+      render :new
+    end
   end
 
-  def index
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method)
   end
 
-  def show
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
