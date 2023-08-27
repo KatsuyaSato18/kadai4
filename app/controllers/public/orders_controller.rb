1759d9207f97aefea8ca1afb2f6dadbe6e9807db
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-08-27 14:50:03 +0000

    8.27

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,15 +7,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @customer = current_customer
-    @order = Order.new
-    @order.postal_code = @customer.postal_code
-    @order.address = @customer.address
-    @order.name = "#{@customer.last_name} #{@customer.first_name}"
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
     if @order.save
-      # 保存に成功した場合の処理
-      redirect_to order_path(@order)
+      cart_items.each do |cart|
+        order_detail = OrderItem.new
+        order_detail.item_id = cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.order_amount = cart.amount
+        order_detail.total_price = cart.item.price
+        order_detail.save
+      end
       flash[:notice] = "注文が完了しました。"
+      redirect_to orders_thanks_path
     else
       render :new
     end
@@ -26,6 +30,9 @@ class Public::OrdersController < ApplicationController
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @order.name = current_customer.first_name + current_customer.last_name
+    @order.postage = 800
+    @cart_items = current_customer.cart_items.all
+    redirect_to orders_check_path
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-
+  has_many :order_details, dependent: :destroy
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true, presence: true, numericality: { greater_than_or_equal_to: 0 }
