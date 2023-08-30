74ae838092744f3a61f2f863d6e54d1d86515ec3
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-08-30 14:51:12 +0000

    8.30

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,33 +9,41 @@ class Public::OrdersController < ApplicationController
   def create
     cart_items = current_customer.cart_items.all
     @order = current_customer.orders.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
+    @order.postage = 800
+    @order.total_price = cart_items.inject(0) { |sum, item| sum + item.sum_price }
+
     if @order.save
       cart_items.each do |cart|
-        order_detail = OrderItem.new
+        order_detail = OrderDetail.new
         order_detail.item_id = cart.item_id
         order_detail.order_id = @order.id
-        order_detail.order_amount = cart.amount
-        order_detail.total_price = cart.item.price
+        order_detail.amount = cart.amount
+        order_detail.price = cart.item.price
         order_detail.save
       end
       cart_items.destroy_all
+      redirect_to orders_thanks_path
     else
-      @order = Order.new(order_params)
       render :new
     end
   end
 
   def check
-    if @order = Order.new(order_params)
+    if params[:order] && params[:order][:payment_method]
+      @order = Order.new(order_params)
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
       @order.postage = 800
       @cart_items = current_customer.cart_items.all
       @total_price = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
+    # ビューに移動する処理を書く
     else
       flash[:notice] = "お支払い方法を選択してください。"
-      render :new
+      redirect_to new_order_path # または適当なパス
     end
   end
 
@@ -43,7 +51,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :payment_method)
+    params.fetch(:order, {}).permit(:postal_code, :address, :name, :payment_method)
   end
 
   def set_order
